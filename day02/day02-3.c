58fc8c8ef6b4f2c443995a4796a9802d959919eb
Lambda <lambda@smail.kongju.ac.kr>
2024-09-19 13:26:28 +0900

    Update day02-3.c

--- day02/day02-3.c
+++ day02/day02-3.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main() {
+	int cnt = 1;
 
 	for (int i = 0; i<= 99 ;i++) {
-		if ((i % 3 == 0 && i % 4 == 0) || i % 7 == 0) {
+		if (i % 12 == 0 /*(i % 3 == 0 && i % 4 == 0)*/ || i % 7 == 0) {
 			printf("%2d ", i);
+			if (cnt == 6) {
+				printf("\n");
+				cnt = 0;
+			}
+
+			cnt++;
 		}
 	}
 
