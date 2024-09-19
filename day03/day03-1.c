bb50cbc17526f64fa49bb227885ca116d8baf70a
Lambda <lambda@smail.kongju.ac.kr>
2024-09-19 14:21:08 +0900

    Update day03-1.c

--- day03/day03-1.c
+++ day03/day03-1.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int check_prime(int num) {
-	int cnt = 0;
+int is_prime_number(int num) {
+	//int cnt = 0;
 
 	for (int i = 2; i < num;i++) {
 		if (num%i==0) {
-			cnt++;
+			return 0; //cnt++;
 		}
 	}
 
+	return 1;
+
+	/*
 	if (cnt == 0) {
 		return 1;
 	}
-
-	return 0;
+	*/
 }
 
 int main() {
@@ -22,12 +24,33 @@ int main() {
 	printf("소수인지 확인할 수를 입력하세요 : ");
 	scanf_s("%d", &num);
 
-	if (check_prime(num) == 1) {
+	if (is_prime_number(num) == 1) {
 		printf("해당 수는 소수입니다.\n");
 	}
 	else {
 		printf("해당 수는 소수가 아닙니다.\n");
 	}
+
+
+	/*
+	int  cnt = 1;
+
+	printf("수 입력 : ");
+	scanf_s("%d", &num);
+
+	for (int i = 1; i <= num; i++) {
+		if (is_prime_number(i) == 1) {
+			printf(" %7d", i);
+
+			if (cnt == 10) {
+				printf("\n");
+				cnt = 0;
+			}
+
+			cnt++;
+		}
+	}
+	*/
 
 	
 	return 0;
