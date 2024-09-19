39490ecbe2aff8f8496433808d0b6aac8f2b07f2
Lambda <lambda@smail.kongju.ac.kr>
2024-09-19 14:26:00 +0900

    Create day03-2.c

---
+++ day03/day03-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int num) {
+	if (num > 1) {
+		return num * factorial(num - 1);
+	}
+	else {
+		return 1;
+	}
+
+}
+
+int main() {
+	int num;
+	printf("계산할 팩토리얼값 입력 : ");
+	scanf_s("%d", &num);
+
+	int result = factorial(num);
+	printf("%d! = %d", num, result);
+}
