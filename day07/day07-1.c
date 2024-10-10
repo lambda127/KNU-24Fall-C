cd2f2925ca5749d9987f1b02417e568d57856da9
Lambda <lambda@smail.kongju.ac.kr>
2024-10-10 14:30:14 +0900

    +comment

--- day07/day07-1.c
+++ day07/day07-1.c
@@ -3,27 +3,31 @@
 int main() {
 
 	int n;
+	
+	do {
+		// 피라미드 층 수 입력
+		printf("층을 입력하세요 : ");
+		scanf_s("%d", &n);
 
-	printf("층을 입력하세요 : ");
-	scanf_s("%d", &n);
+		if (n < 1) printf("층 수는 1보다 작을 수 없습니다.\n");
+	} while (n < 1);  // 입력한 층 수가 1 미만일 때, 층 수 다시 입력
 
-	if (n >= 1) {
-		
-		printf("%d층 피라미드\n", n);
 
-		for (int i = 1; i <= n; i++) {
+	printf("%d층 피라미드\n", n); // 피라미드 층 수 표시
 
-			for (int j = i; j < n; j++) {
-				printf(" ");
-			}
-			
-			for (int j = 1; j <= (2 * i - 1); j++) {
-				printf("*");
-			}
+	for (int i = 1; i <= n; i++) { // 각 층 마다 별을 찍기 위한 반복
 
-			printf("\n");
-
+		// 공백 출력
+		for (int j = i; j < n; j++) { // 공백의 개수는 n-i (i는 층..ex: 1층 -> n-1, 2층 -> n-2 .... n층 -> n-n = 0 
+			printf(" ");
 		}
+
+		// '*' 출력
+		for (int j = 1; j <= (2 * i - 1); j++) { // 각 층별 별 개수는 2 * (별 개수) - 1
+			printf("*");
+		}
+
+		printf("\n");
 
 	}
 
--- day07/day07-2.c
+++ day07/day07-2.c
@@ -1,54 +1,54 @@
 
-//Monte Carlo Algorithm
+// Monte Carlo Algorithm
 
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define BILLION 1000000000
+#define BILLION 1000000000  // 10억 상수 정의
 
 int main() {
 
-	double x = 0;
-	double y = 0;
-	double pi = 0;
+	double x = 0;	// 찍힐 점의 x좌표
+	double y = 0;	// 찍힐 점의 y좌표
+	double pi = 0;	// 파이 값 계산 시 저장
 	
 
-	int count = 1, circle = 0;
-	int rate = 0;
+	int count = 1, circle = 0;	// count -> 시행 횟수 저장, circle -> 원 안에 들어간 점의 개수 저장
+	int rate = 0;				// 진행률 저장
 
 	srand(time(NULL));
 
-	while (count <= BILLION) {
+	while (count <= BILLION) { // 10억번 시행
 
-		x = (double)rand() / (double)RAND_MAX;
-		y = (double)rand() / (double)RAND_MAX;
+		x = (double)rand() / (double)RAND_MAX;	// x 값 랜덤 생성
+		y = (double)rand() / (double)RAND_MAX;	// y 값 램덤 생성
 
-		if (sqrt(pow(x, 2) + pow(y, 2)) < 1) circle++;
+		if (sqrt(pow(x, 2) + pow(y, 2)) <= 1) circle++;	// 원의 범위 (반지름 1) 내에 들어갔을 시 circle 1 증갖
 
-		if (count % (BILLION/100) == 0) {
-			rate = count / (BILLION / 100);
+		if (count % (BILLION/100) == 0) {	// BILLION/100 -> 1000만 으로 마누어 떨어지면... => 1000만번 마다 실행
+			rate = count / (BILLION / 100);	// 진행률
 
-			pi = 4 * ((double)circle / (double)count);
+			pi = 4 * ((double)circle / (double)count);	// Monte Carlo Algorithm -> 4 * (원 안에 들어간 점의 개수) / (찍은 점의 개수)
 
-			printf("%d%%진행.. 원주율 : %lf ", rate, pi);
+			printf("%d%%진행.. 원주율 : %lf ", rate, pi);	// 진행률, 계산된 파이 값 계산
 
-			for (int i = 0; i < 20; i++) {
+			for (int i = 0; i < 20; i++) {	// 총 20칸으로 진행도 표시 -> 한 칸당 5% 차지
 
-				if (i < (rate / 5)) {
-					printf("■");
+				if (i < (rate / 5)) {	// 진행률을 5로 나누면 최대 20칸 차지 가능
+					printf("■"); // i가 조건에 부합하는 경우 -> 진행율을 5로 나눴을 떄 1, 2, 3,...20이 되는데 i의 값이 이 값보다 작은 경우 '■' 출력 
 				}
 				else {
-					printf("□");
+					printf("□"); // 아니면 '□'
 				}
 
 			}
 			printf("\n");
 		}
-		count++;
+		count++; // 시행 횟수 증가
 	}
-	printf("원주율 ; %lf\n", pi);
+	printf("원주율 ; %lf\n", pi);	// 최종 결과 출력
 
 
 	return 0;
